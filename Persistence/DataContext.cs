acbd57c377e67ce188c349dd557b4ee98ca68b0b
Ched Cheatham <ched.cheatham@gmail.com>
2020-08-15 09:08:17 -0500

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base(options) {
+
+        }
+
+        public DbSet<Value> Values { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1, Name="Hello, Ched"},
+                    new Value {Id=2, Name="Hello, Angie"},
+                    new Value {Id=3, Name="Hello, Kyndal"},
+                    new Value {Id=4, Name="Hello, Brantly"},
+                    new Value {Id=5, Name="Hello, Noah"}
+                );
+        }
+    }
+}
---
+++ Persistence/Migrations/20200815115205_SeedValuesMore.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValuesMore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Hello, Ched");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Hello, Angie");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Hello, Kyndal");
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "Hello, Brantly" });
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 5, "Hello, Noah" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Hellow");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Hellow");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Hellow");
+        }
+    }
+}
